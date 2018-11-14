57efeef684de6aea996ef0859e6a6a2badbafb5d
hsue66 <dufmaakedl@gmail.com>
2018-11-14 19:35:48 +0900

    ejs

--- Sec26_EJS/EJSDemo/app.js
+++ Sec26_EJS/EJSDemo/app.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine","ejs");
+
 app.get("/",function(req,res){
-  res.render("home.ejs");
+  res.render("home");
 });
 
 app.get("/fallinlovewith/:thing",function(req,res){
   var thing = req.params.thing;
-  res.render("fall.ejs",{thingVar: thing});
+  res.render("fall",{thingVar: thing});
 });
 
 app.get("/posts", function(req,res){
@@ -16,7 +19,7 @@ app.get("/posts", function(req,res){
     {title: "Opps", author:"ops"},
     {title: "Aha", author:"aha"},
   ];
-  res.render("post.ejs",{posts:posts});
+  res.render("post",{posts:posts});
 });
 
 app.listen(3000,function(){
